688064b6be8598e0c35d64063c46633a9986606f
cn-kali-team <kali-team@qq.com>
2026-05-17 09:38:29 +0800

    update

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,11 @@
 use thiserror::Error;
 
 #[allow(dead_code)]
-pub type Result<T> = ::std::result::Result<T, {{crate_name | pascal_case}}Error>;
+pub type Result<T> = ::std::result::Result<T, Error>;
 
 #[allow(dead_code)]
 #[derive(Debug, Error)]
-pub enum {{crate_name | pascal_case}}Error {
-    #[error("some error: '{0}'")]
-    SomeError(String),
+pub enum Error {
+  #[error("some error: '{0}'")]
+  SomeError(String),
 }
